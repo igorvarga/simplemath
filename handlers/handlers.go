917d1c5996004eb6f1169f147734fac3ef83c1f8
Igor Varga <igor@oblivion.hr>
2020-10-14 00:19:00 +0200

    Second version of all math handlers, add table tests

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,52 +2,89 @@ package handlers
 
 import (
 	"fmt"
+	sm "github.com/igorvarga/teletchcodechallenge/simplemath"
 	"net/http"
 	"strconv"
 )
+
 // TODO Refactor JSON building
 func AddHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO Extract x, y check to function
-	x, err := strconv.ParseFloat(r.URL.Query().Get("x"), 64)
+	x, y, err := extractParams(w, r)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		response := fmt.Sprintf(`{"code": %v, "message": "%v"}`, 1, err.Error())
-		fmt.Fprintf(w, response)
 		return
 	}
 
-	y, err := strconv.ParseFloat(r.URL.Query().Get("y"), 64)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		response := fmt.Sprintf(`{"code": %v, "message": "%v"}`, 1, err.Error())
-		fmt.Fprintf(w, response)
-		return
-	}
-
-	answer := x + y
+	answer := sm.Add(x, y)
 
 	response := fmt.Sprintf(`{"action": "add", "x": %v, "y": %v, "answer", %v, "cached": false}`, x, y, answer)
 
 	w.WriteHeader(http.StatusOK)
 
 	fmt.Fprintf(w, response)
-
 }
 
 func SubtractHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `{"result": true}`)
+	x, y, err := extractParams(w, r)
+	if err != nil {
+		return
+	}
+
+	answer := sm.Subtract(x, y)
+
+	response := fmt.Sprintf(`{"action": "subtract", "x": %v, "y": %v, "answer", %v, "cached": false}`, x, y, answer)
+
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w, response)
 }
 
 func DivideHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `{"result": true}`)
+	x, y, err := extractParams(w, r)
+	if err != nil {
+		return
+	}
+
+	answer := sm.Divide(x, y)
+
+	response := fmt.Sprintf(`{"action": "divide", "x": %v, "y": %v, "answer", %v, "cached": false}`, x, y, answer)
+
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w, response)
 }
 
 func MultiplyHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `{"result": true}`)
+	x, y, err := extractParams(w, r)
+	if err != nil {
+		return
+	}
+
+	answer := sm.Multiply(x, y)
+
+	response := fmt.Sprintf(`{"action": "multiply", "x": %v, "y": %v, "answer", %v, "cached": false}`, x, y, answer)
+
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w, response)
 }
 
-/*
-func extractParams(r *http.Request) (x float64, y float64, err error) {
+func extractParams(w http.ResponseWriter, r *http.Request) (x float64, y float64, err error) {
 
+	x, err = strconv.ParseFloat(r.URL.Query().Get("x"), 64)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := fmt.Sprintf(`{"code": %v, "message": "%v"}`, 1, err.Error())
+		fmt.Fprintf(w, response)
+		return x, y, err
+	}
+
+	y, err = strconv.ParseFloat(r.URL.Query().Get("y"), 64)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := fmt.Sprintf(`{"code": %v, "message": "%v"}`, 1, err.Error())
+		fmt.Fprintf(w, response)
+		return x, y, err
+	}
+
+	return x, y, err
 }
- */
--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,37 +1,57 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
 )
+var basicmathtests = []struct {
+	x        string
+	y        string
+	expected string
+	handler  http.HandlerFunc
+	name     string
+	action   string
+	path     string
+}{
+	{"2", "5", "7", AddHandler, "AddHandler", "add", "/add"},
+	{"2", "5", "-3", SubtractHandler, "SubtractHandler", "subtract", "/subtract"},
+	{"2", "5", "10", MultiplyHandler, "MultiplyHandler", "multiply", "/multiply"},
+	{"10", "5", "2", DivideHandler, "DivideHandler", "divide", "/divide"},
+}
 
-func TestAddHandler(t *testing.T) {
-	values := url.Values{
-		"x": {"2"},
-		"y": {"5"}}
+func TestMathHandlers(t *testing.T) {
 
-	url := url.URL{Path: "/add", RawQuery: values.Encode()}
+	for _, mt := range basicmathtests {
+		t.Run(mt.name, func(t *testing.T) {
+			values := url.Values{
+				"x": {mt.x},
+				"y": {mt.y}}
 
-	req, err := http.NewRequest(http.MethodGet, url.String(), nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+			url := url.URL{Path: mt.path, RawQuery: values.Encode()}
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(AddHandler)
-	handler.ServeHTTP(rr, req)
+			req, err := http.NewRequest(http.MethodGet, url.String(), nil)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(mt.handler)
+			handler.ServeHTTP(rr, req)
 
-	expected := `{"action": "add", "x": 2, "y": 5, "answer", 7, "cached": false}`
-	if rr.Body.String() != expected {
-		t.Errorf("Handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("Handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+
+			expected := fmt.Sprintf(`{"action": "%v", "x": %v, "y": %v, "answer", %v, "cached": false}`, mt.action, mt.x, mt.y, mt.expected)
+			if rr.Body.String() != expected {
+				t.Errorf("Handler returned unexpected body: got %v want %v",
+					rr.Body.String(), expected)
+			}
+		})
 	}
 }
 
@@ -77,3 +97,32 @@ func TestAddHandlerYMissing(t *testing.T) {
 			status, http.StatusBadRequest)
 	}
 }
+
+func TestSubtractHandler(t *testing.T) {
+	values := url.Values{
+		"x": {"2"},
+		"y": {"5"}}
+
+	url := url.URL{Path: "/add", RawQuery: values.Encode()}
+
+	req, err := http.NewRequest(http.MethodGet, url.String(), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(SubtractHandler)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"action": "subtract", "x": 2, "y": 5, "answer", -3, "cached": false}`
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
